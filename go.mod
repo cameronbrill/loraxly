8e010201142482a84fb545a1dfda1bb59f3f7647
Cameron Brill <c00brill@gmail.com>
2021-02-14 00:51:40 -0700

    get stock data, use openai api, get articles related to the stock.

--- go.mod
+++ go.mod
@@ -3,7 +3,23 @@ module github.com/cameronbrill/melon-watch
 go 1.15
 
 require (
+	github.com/PuerkitoBio/goquery v1.6.1 // indirect
+	github.com/andybalholm/cascadia v1.2.0 // indirect
+	github.com/antchfx/htmlquery v1.2.3 // indirect
+	github.com/antchfx/xmlquery v1.3.4 // indirect
+	github.com/antchfx/xpath v1.1.11 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/jawher/mow.cli v1.2.0 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/sashabaranov/go-gpt3 v0.0.0-20201216121239-64c0048394ea // indirect
 	github.com/sirupsen/logrus v1.7.0 // indirect
+	github.com/temoto/robotstxt v1.1.1 // indirect
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
 	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+	golang.org/x/text v0.3.5 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 	gogpt "github.com/sashabaranov/go-gpt3"
+	"github.com/gocolly/colly"
 )
 
 type StockRes struct {
@@ -25,50 +26,163 @@ type StockRes struct {
 	} `json:"Meta Data"`
 	TimeSeries struct {
 		TimeSeriesData map[string]interface{} `json:"-"`
+		/*
+		2021-02-12 19:50:00": {
+		            "1. open": "120.9000",
+		            "2. high": "120.9000",
+		            "3. low": "120.9000",
+		            "4. close": "120.9000",
+		            "5. volume": "300"
+		        },
+		*/
 	} `json:"Time Series (5min)"`
 }
 
-type returnData struct {
-	Stocks map[string]struct{
-		Details struct {
-			Description string `json:"description"`
-			EnvironmentalSummary string `json:"environmental_summary"`
-		} `json:"details"`
-		TimeData map[string]struct{
-			ClosePrice float64 `json:"close_price"`
-			Volume float64 `json:"volume"`
-			EnvironmentalScore float64 `json:"environmental_score"`
-			RawSentiment float64 `json:"raw_sentiment"`
-		} `json:"time_data"`
-	} `json:"stocks"`
+type StockDetail struct {
+	Ticker string `json:"Symbol"`
+	Name string `json:"Name"`
+	Description string `json:"Description"`
+}
+
+type StockAPIRes struct {
+	StockRes
+	StockDetail
+}
+
+type TimeSeries struct {
+	ClosePrice float64 `json:"close_price"`
+	Volume float64 `json:"volume"`
+}
+
+type Stock struct {
+	Details struct {
+		Description string `json:"description"`
+		EnvironmentalSummary string `json:"environmental_summary"`
+		EnvironmentalArticles map[string]*string `json:"environmental_articles"`
+		RawSentiment string `json:"raw_sentiment"`
+		EnvironmentalScore float64 `json:"environmental_score"`
+		EnvironmentalSentiment []string `json:"environmental_sentiment"`
+	} `json:"details"`
+	TimeData map[string]*TimeSeries `json:"time_data"`
+}
+
+type ReturnData struct {
+	Stocks map[string]*Stock `json:"stocks"`
 }
 
 var (
-	STOCK_ENDPOINT="https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&outputsize=full&symbol=%s&interval=5min&apikey=%s"
-	STOCK_API_KEY=os.Getenv("ALPHA_KEY")
-	OPEN_AI_API_KEY=os.Getenv("OPEN_AI_KEY")
-	STOCK_LIST_FILE="stocks.csv"
+	STOCK_TIME_API_ENDPOINT   ="https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&outputsize=full&symbol=%s&interval=5min&apikey=%s"
+	STOCK_DETAIL_API_ENDPOINT ="https://www.alphavantage.co/query?function=OVERVIEW&symbol=%s&apikey=%s"
+	STOCK_API_KEY             =os.Getenv("ALPHA_KEY")
+	OPEN_AI_API_KEY           =os.Getenv("OPEN_AI_KEY")
+	STOCK_LIST_FILE           ="stocks.csv"
+	SEARCH_URL="https://duckduckgo.com/?q=%s+environmental+impact" // replace any space with '+'
 )
 
 func main() {
 	tickers := make(chan string, 52)
-	stocks := make(chan *StockRes, 52)
+	stocks := make(chan *StockAPIRes, 52)
 	go getStockTickers(tickers)
-	go func(tickers chan string, stocks chan *StockRes) {
+	go func(tickers chan string, stocks chan *StockAPIRes) {
 		for {
 			ticker, ok := <-tickers
-			log.Infof("TICKER, OK: %s, %v", ticker, ok)
 			if !ok {
 				return
 			}
-			go func(tickers chan string, stocks chan *StockRes) {
-				stocks <- getTickerData(fmt.Sprintf("%s", ticker))
+			go func(tickers chan string, stocks chan *StockAPIRes) {
+				stockRes := getTickerData(fmt.Sprintf("%s", ticker))
+				stockDetails := getTickerDetails(fmt.Sprintf("%s", ticker))
+				stockTotal := &StockAPIRes{
+					*stockRes,
+					*stockDetails,
+				}
+				stocks <- stockTotal
 				log.Infof("got stock %s", ticker)
 				return
 			}(tickers, stocks)
 		}
 	}(tickers, stocks)
+	stockReturn := ReturnData{}
+	stockReturn.Stocks = make(map[string]*Stock)
 	openAIClient := gogpt.NewClient(OPEN_AI_API_KEY)
+	ctx := context.Background()
+	for stock := range stocks {
+		log.Printf("STOCK IS %+v, STOCK DETAIL IS %+v", stock, stock.StockDetail)
+		stockReturn.Stocks[stock.StockDetail.Ticker] = &Stock{}
+		stockReturn.Stocks[stock.StockDetail.Ticker].Details.Description = stock.Description
+		req := gogpt.CompletionRequest{
+			MaxTokens: 1,
+			Prompt: fmt.Sprintf(`Description: "%s"\nSentiment (positive, neutral, negative):`, stock.Description),
+		}
+		sentimentRes, err := openAIClient.CreateCompletion(ctx, "davinci-instruct-beta", req)
+		if err != nil {
+			log.Errorf("failed to get sentiment from OpenAI for ticker %s", stock.StockDetail.Ticker)
+		}
+		rawSentiment := sentimentRes.Choices[0].Text
+		stockReturn.Stocks[stock.StockDetail.Ticker].Details.RawSentiment = rawSentiment
+
+		c := colly.NewCollector(
+			colly.MaxDepth(1),
+			colly.Async(true),
+			)
+		detailCollector := c.Clone()
+		// Find and visit all links
+		c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+			if e.Attr("class") == "result__url" {
+				placeholder := ""
+				stockReturn.Stocks[stock.StockDetail.Ticker].Details.EnvironmentalArticles[e.Attr("href")] = &placeholder
+				e.Request.Visit(e.Attr("href"))
+			}
+		})
+
+		detailCollector.OnHTML("p", func(e *colly.HTMLElement) {
+			childText := *stockReturn.Stocks[stock.StockDetail.Ticker].Details.EnvironmentalArticles[e.Request.URL.String()] + "\n" + e.ChildText("p")
+			stockReturn.Stocks[stock.StockDetail.Ticker].Details.EnvironmentalArticles[e.Request.URL.String()] = &childText
+		})
+
+		detailCollector.OnHTML("span", func(e *colly.HTMLElement) {
+			childText := *stockReturn.Stocks[stock.StockDetail.Ticker].Details.EnvironmentalArticles[e.Request.URL.String()] + "\n" + e.ChildText("span")
+			stockReturn.Stocks[stock.StockDetail.Ticker].Details.EnvironmentalArticles[e.Request.URL.String()] = &childText
+		})
+
+		c.OnRequest(func(r *colly.Request) {
+			log.Infof("Visiting %s", r.URL.String())
+		})
+		log.Infof("STOCK TICKER IS %v", stock.StockDetail.Ticker)
+		c.Visit(fmt.Sprintf(SEARCH_URL, stock.StockDetail.Ticker))
+		c.Wait()
+
+		for k, v := range stockReturn.Stocks[stock.StockDetail.Ticker].Details.EnvironmentalArticles {
+			req = gogpt.CompletionRequest{
+				MaxTokens: 1,
+				Prompt: fmt.Sprintf(`%s\n\ntl;dr:`, v),
+			}
+			summaryRes, err := openAIClient.CreateCompletion(ctx, "davinci-instruct-beta", req)
+			if err != nil {
+				log.Errorf("failed to get sentiment from OpenAI for ticker %s, url %s", stock.StockDetail.Ticker, k)
+			}
+			summary := summaryRes.Choices[0].Text
+			req = gogpt.CompletionRequest{
+				MaxTokens: 1,
+				Prompt: fmt.Sprintf(`Description: "%s"\nSentiment (positive, neutral, negative):`, summary),
+			}
+			sentimentURLRes, err := openAIClient.CreateCompletion(ctx, "davinci-instruct-beta", req)
+			if err != nil {
+				log.Errorf("failed to get sentiment from OpenAI for ticker %s, url %s", stock.StockDetail.Ticker, k)
+			}
+			rawURLSentiment := sentimentURLRes.Choices[0].Text
+			stockReturn.Stocks[stock.StockDetail.Ticker].Details.EnvironmentalSentiment = append(stockReturn.Stocks[stock.StockDetail.Ticker].Details.EnvironmentalSentiment, rawURLSentiment)
+			log.Infof("sentiment of article %s is %s", k, rawURLSentiment)
+		}
+		/*
+		for past 15 articles (search by stock.name+environmental impact):
+			summarize
+			sentiment on summaries
+		equations
+		transform time series to return time
+		post to firebase
+		*/
+	}
 	select {}
 }
 
@@ -100,7 +214,7 @@ func getStockTickers(list chan string) {
 
 func getTickerData(ticker string) *StockRes {
 	log.Infof("getting stock data for %s ticker...", ticker)
-	resp, err := http.Get(fmt.Sprintf(STOCK_ENDPOINT, ticker, STOCK_API_KEY))
+	resp, err := http.Get(fmt.Sprintf(STOCK_TIME_API_ENDPOINT, ticker, STOCK_API_KEY))
 	if err != nil {
 		log.Errorf("failed to get stock data for %s ticker: %s", ticker, err)
 	}
@@ -117,6 +231,24 @@ func getTickerData(ticker string) *StockRes {
 
 	json.Unmarshal([]byte(respString), &stock.TimeSeries.TimeSeriesData)
 	log.Infof("unmarshalled stock time series for %s ticker...", ticker)
+
+	return &stock
+}
+
+func getTickerDetails(ticker string) *StockDetail {
+	log.Infof("getting stock details for %s ticker...", ticker)
+	resp, err := http.Get(fmt.Sprintf(STOCK_DETAIL_API_ENDPOINT, ticker, STOCK_API_KEY))
+	if err != nil {
+		log.Errorf("failed to get stock details for %s ticker: %s", ticker, err)
+	}
+
+	defer resp.Body.Close()
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+
+	// Convert response body to StockRes struct
+	var stock StockDetail
+	json.Unmarshal(bodyBytes, &stock)
+	log.Infof("unmarshalled stock details for %s ticker...", ticker)
 
 	return &stock
 }
